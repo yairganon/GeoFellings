00a851486779054636bd9ceaa3314083cdc8505e
yairg <yairg@wix.com>
2018-01-09 18:22:03 +0200

    return status when register

--- web/src/main/scala/spring/controllers/UsersController.scala
+++ web/src/main/scala/spring/controllers/UsersController.scala
@@ -1,9 +1,11 @@
 package spring.controllers
 
+import enums.RegisterStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestBody, RequestMapping, RequestMethod, ResponseBody}
 import service.dto.{UserLoginRequest, UserRegisterRequest}
 import util.UserId
+import views.RegisterStatusView
 
 @Controller
 @RequestMapping(Array("/api/user"))
@@ -18,7 +20,10 @@ class UsersController {
 
   @RequestMapping(method = Array(RequestMethod.POST), value = Array("register"))
   @ResponseBody
-  def register(@RequestBody request: UserRegisterRequest): Unit = {
-    print(request)
+  def register(@RequestBody request: UserRegisterRequest): RegisterStatusView = {
+    if(request.userName == "1")
+      RegisterStatusView(RegisterStatus.CONFLICT)
+    else
+      RegisterStatusView(RegisterStatus.CREATED)
   }
 }
--- web/src/main/scala/views/QuestionsViews.scala
+++ web/src/main/scala/views/QuestionsViews.scala
@@ -1,9 +1,11 @@
 package views
 
-import enums.QuestionType
+import enums.{QuestionType, RegisterStatus}
 
 case class QuestionsView(questions: Seq[QuestionView])
 
 case class QuestionView(id: String, questionType: QuestionType, data: QuestionDataView)
 
 case class QuestionDataView(questionString: String, numOfOptions: Option[Int])
+
+case class RegisterStatusView(status: RegisterStatus)
