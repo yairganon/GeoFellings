a86b9ff4b24e0b66a28475995d317548ba8ceca8
yairg <yairg@wix.com>
2018-01-12 18:12:07 +0200

    add view to QuestionnaireWithAnswersView

--- web/src/main/scala/views/QuestionsViews.scala
+++ web/src/main/scala/views/QuestionsViews.scala
@@ -13,17 +13,32 @@ case class RegisterStatusView(status: RegisterStatus)
 
 case class QuestionnairesView(questionnaires: Seq[QuestionnaireView])
 
-case class QuestionnaireView(
-  id: String,
-  name: String,
-  isRegistration: Boolean,
-  isDefault: Boolean,
-  questions: Seq[QuestionView]
-)
+case class QuestionnaireView(id: String,
+                             name: String,
+                             isRegistration: Boolean,
+                             isDefault: Boolean,
+                             questions: Seq[QuestionView])
 
 case class UserView(userId: UserId,
                     userName: String,
                     gender: Gender,
                     age: Int)
+
+case class QuestionWithAnswersView(id: String,
+                                   questionType: QuestionType,
+                                   data: QuestionDataView,
+                                   answer: String)
+
+case class QuestionnaireWithAnswersView(id: String,
+                                        name: String,
+                                        isRegistration: Boolean,
+                                        isDefault: Boolean,
+                                        questions: Seq[QuestionWithAnswersView])
+
+case class FullUserView(userId: UserId,
+                        userName: String,
+                        gender: Gender,
+                        age: Int,
+                        questionnaires: Seq[QuestionnaireWithAnswersView])
 
 case class UsersView(users: Seq[UserView])
