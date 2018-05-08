aa9675f2ab4e887ebb760eb1d616d4334b10e22e
yairg <yairg@wix.com>
2018-05-08 23:36:10 +0300

    create tables

--- core/src/main/scala/coreLogic/repos/mySql/JdbcConnectSelect.scala
+++ core/src/main/scala/coreLogic/repos/mySql/JdbcConnectSelect.scala
@@ -14,7 +14,9 @@ object JdbcConnectSelect {
   try {
     Class.forName(driver)
     connection = DriverManager.getConnection(url, username, password)
-    val schema = scala.io.Source.fromURL(getClass.getResource("db/schema.sql")).mkString
+
+
+    val schema = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("db/schema.sql")).mkString
     val statement = connection.createStatement()
     statement.executeLargeUpdate(schema)
   } catch {
