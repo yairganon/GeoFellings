e783c93c17e5331e0421e9894e99470a148cfaf8
yairg <yairg@wix.com>
2018-05-08 23:21:51 +0300

    create tables

--- core/src/main/scala/coreLogic/repos/mySql/JdbcConnectSelect.scala
+++ core/src/main/scala/coreLogic/repos/mySql/JdbcConnectSelect.scala
@@ -15,16 +15,11 @@ object JdbcConnectSelect {
     Class.forName(driver)
     connection = DriverManager.getConnection(url, username, password)
     val statement = connection.createStatement()
-    val resultSet = statement.executeQuery("CREATE DATABASE IF NOT EXISTS `geoFeelings`;\n\nCREATE TABLE IF NOT " +
+    statement.executeUpdate("CREATE DATABASE IF NOT EXISTS `geoFeelings`;\n\nCREATE TABLE IF NOT " +
       "EXISTS `geoFeelings.questions` (\n  `questionId` varchar(50) NOT NULL,\n  `data` mediumblob,\n  " +
       "`creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,\n  `update_date` timestamp NOT NULL DEFAULT " +
       "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n  PRIMARY KEY (`questionId`)\n) ENGINE=InnoDB DEFAULT " +
       "CHARSET=utf8;")
-    while(resultSet.next()) {
-      val host = resultSet.getString("host")
-      val user = resultSet.getString("user")
-      println("host, user = " + host + ", " + user)
-    }
   } catch {
     case e: Throwable => e.printStackTrace()
   }
