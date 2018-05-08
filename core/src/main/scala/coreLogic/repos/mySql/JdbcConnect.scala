10747c68ec42acda881745b251c5082f0afa05c0
yairg <yairg@wix.com>
2018-05-09 00:30:04 +0300

    create tables

--- core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
+++ core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
@@ -1,7 +1,7 @@
 package coreLogic.repos.mySql
-
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.datasource.DriverManagerDataSource
+//
+//import org.springframework.jdbc.core.JdbcTemplate
+//import org.springframework.jdbc.datasource.DriverManagerDataSource
 
 object JdbcConnect {
 //
