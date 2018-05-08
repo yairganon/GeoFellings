37632b2e2adf1ff8d3acf1fd8d60bb3eaee9ef28
yairg <yairg@wix.com>
2018-05-09 00:01:57 +0300

    create tables

--- core/src/main/resources/db/schema.sql
+++ core/src/main/resources/db/schema.sql
@@ -17,3 +17,72 @@ CREATE TABLE IF NOT EXISTS geoFeelings.questionnaire (
   PRIMARY KEY (`questionnaireId`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.questionnaireAnswer (
+  `userId` varchar(50) NOT NULL,
+  `questionnaireId` varchar(50) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` mediumblob,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`, `questionnaireId`, `creation_date`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.users (
+  `userId` varchar(50) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `passWord` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userName`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.userLocations (
+  `userId` varchar(50) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` mediumblob,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`, `creation_date`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.userNotifications (
+  `userId` varchar(50) NOT NULL,
+  `questionnaireId` varchar(50) NOT NULL,
+  `notficationStatus` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`, `questionnaireId`, `creation_date`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.triggers (
+  `questionnaireId` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`questionnaireId`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.twitterTokens (
+  `userId` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.facebookTokens (
+  `userId` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
