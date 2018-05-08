07817e52d9fdd4b7183486719383a6ffed192cd0
yairg <yairg@wix.com>
2018-05-08 23:49:28 +0300

    create tables

--- core/src/main/resources/db/schema.sql
+++ core/src/main/resources/db/schema.sql
@@ -1,10 +1,19 @@
-CREATE DATABASE IF NOT EXISTS `geoFeelings`;
+CREATE DATABASE IF NOT EXISTS geoFeelings;
 
-CREATE TABLE IF NOT EXISTS `geoFeelings.questions` (
+CREATE TABLE IF NOT EXISTS geoFeelings.questions (
   `questionId` varchar(50) NOT NULL,
   `data` mediumblob,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`questionId`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS geoFeelings.questionnaire (
+  `questionnaireId` varchar(50) NOT NULL,
+  `data` mediumblob,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`questionnaireId`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
