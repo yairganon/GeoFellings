147ced30ee63a814362c2177ffdd77db1d00f454
yairg <yairg@wix.com>
2018-05-09 00:41:31 +0300

    create tables

--- core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
+++ core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
@@ -3,31 +3,28 @@ package coreLogic.repos.mySql
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 
+import scala.io.Source._
+
 object JdbcConnect {
 
-  val driver = "com.mysql.jdbc.Driver"
-  val url = "jdbc:mysql://localhost/mysql"
-  val username = "root"
-  val password = "1q2w3e4r"
-
-  try {
-    val schema = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("db/schema.sql"))
-      .mkString.replace("\n", "")
-      .split(";")
-      .filter(_.nonEmpty)
-    Class.forName(driver)
-    val dataSource = new DriverManagerDataSource
-    dataSource.setDriverClassName(driver)
-    dataSource.setUrl(url)
-    dataSource.setUsername(username)
-    dataSource.setPassword(password)
-    val storageTemplate: JdbcTemplate = new JdbcTemplate(dataSource)
-
-    schema.foreach(storageTemplate.execute)
-  } catch {
-    case e: Throwable =>
-      println("*" * 200)
-      e.printStackTrace()
-      println("*" * 200)
+  private val driver = "com.mysql.jdbc.Driver"
+  private val url = "jdbc:mysql://localhost/mysql"
+  private val username = "root"
+  private val password = "1q2w3e4r"
+  private val dataSource = {
+    val builder = new DriverManagerDataSource
+    builder.setDriverClassName(driver)
+    builder.setUrl(url)
+    builder.setUsername(username)
+    builder.setPassword(password)
+    builder
   }
+  private val schema = fromInputStream(getClass.getClassLoader.getResourceAsStream("db/schema.sql"))
+    .mkString
+    .replace("\n", "")
+    .split(";")
+    .filter(_.nonEmpty)
+  private val storageTemplate = new JdbcTemplate(dataSource)
+  schema.foreach(storageTemplate.execute)
+
 }
