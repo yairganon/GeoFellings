693309ca5b23cb9fc1dab5dd7457ab9055f42272
yairg <yairg@wix.com>
2018-05-09 00:31:15 +0300

    create tables

--- core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
+++ core/src/main/scala/coreLogic/repos/mySql/JdbcConnect.scala
@@ -1,33 +1,33 @@
 package coreLogic.repos.mySql
-//
-//import org.springframework.jdbc.core.JdbcTemplate
-//import org.springframework.jdbc.datasource.DriverManagerDataSource
+
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.DriverManagerDataSource
 
 object JdbcConnect {
-//
-//  val driver = "com.mysql.jdbc.Driver"
-//  val url = "jdbc:mysql://localhost/mysql"
-//  val username = "root"
-//  val password = "1q2w3e4r"
-//
-//  try {
-//    val schema = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("db/schema.sql"))
-//      .mkString.replace("\n", "")
-//      .split(";")
-//      .filter(_.nonEmpty)
-//    Class.forName(driver)
-//    val dataSource = new DriverManagerDataSource
-//    dataSource.setDriverClassName(driver)
-//    dataSource.setUrl(url)
-//    dataSource.setUsername(username)
-//    dataSource.setPassword(password)
-//    val storageTemplate: JdbcTemplate = new JdbcTemplate(dataSource)
-//
-//    schema.foreach(storageTemplate.execute)
-//  } catch {
-//    case e: Throwable =>
-//      println("*" * 200)
-//      e.printStackTrace()
-//      println("*" * 200)
-//  }
+
+  val driver = "com.mysql.jdbc.Driver"
+  val url = "jdbc:mysql://localhost/mysql"
+  val username = "root"
+  val password = "1q2w3e4r"
+
+  try {
+    val schema = scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("db/schema.sql"))
+      .mkString.replace("\n", "")
+      .split(";")
+      .filter(_.nonEmpty)
+    Class.forName(driver)
+    val dataSource = new DriverManagerDataSource
+    dataSource.setDriverClassName(driver)
+    dataSource.setUrl(url)
+    dataSource.setUsername(username)
+    dataSource.setPassword(password)
+    val storageTemplate: JdbcTemplate = new JdbcTemplate(dataSource)
+
+    schema.foreach(storageTemplate.execute)
+  } catch {
+    case e: Throwable =>
+      println("*" * 200)
+      e.printStackTrace()
+      println("*" * 200)
+  }
 }
