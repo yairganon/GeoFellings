8f83f5fb8a53a4ed509c3d759e3e1b597032c503
yairg <yairg@wix.com>
2018-05-13 19:36:41 +0300

    create USer mySql

--- web/src/main/scala/spring/controllers/AppUsersController.scala
+++ web/src/main/scala/spring/controllers/AppUsersController.scala
@@ -10,6 +10,8 @@ import util.{QuestionnaireId, UserId}
 import views.ToViews._
 import views.{QuestionnaireView, QuestionnairesView, RegisterStatusView, UserView}
 
+import scala.util.{Failure, Try}
+
 @Controller
 @RequestMapping(Array("/api/user"))
 class AppUsersController(registrationService: RegistrationService,
@@ -17,15 +19,22 @@ class AppUsersController(registrationService: RegistrationService,
                          userService: UserService,
                          triggerService: TriggerService) {
 
-  private val qId1 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.RADIO, CreateQuestionData("Question-1", Some(7), None)))
-  private val qId2 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.OPEN, CreateQuestionData("Question-2", None, None)))
-  private val qId3 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.MULTIPLE, CreateQuestionData("Question-3", None, Some(Seq("opt-1", "opt-2", "opt-3")))))
-    private val id1 = questionsService.addQuestionnaire(CreateQuestionnaireRequest("Questionnaire-1", true, true, Seq(qId1, qId2, qId3)))
-  private val id2 = questionsService.addQuestionnaire(CreateQuestionnaireRequest("Questionnaire-2", true, true, Seq(qId1, qId2, qId3)))
-//  registrationService.registerUser(UserRegisterRequest("1", "1", Gender.MALE, 26, None))
-  triggerService.addTrigger(CreateTriggerRequest("Trigger Name", id1, None, Some(SocialNetworkTrigger(true)), None))
-  questionsService.addQuestionnaireTo(userService.getAllUser()(0).userId, id1)
-  questionsService.addQuestionnaireTo(userService.getAllUser()(0).userId, id2)
+  Try{
+    val qId1 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.RADIO, CreateQuestionData("Question-1", Some(7), None)))
+    val qId2 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.OPEN, CreateQuestionData("Question-2", None, None)))
+    val qId3 = questionsService.addQuestion(CreateQuestionRequest(QuestionType.MULTIPLE, CreateQuestionData("Question-3", None, Some(Seq("opt-1", "opt-2", "opt-3")))))
+    val id1 = questionsService.addQuestionnaire(CreateQuestionnaireRequest("Questionnaire-1", true, true, Seq(qId1, qId2, qId3)))
+    val id2 = questionsService.addQuestionnaire(CreateQuestionnaireRequest("Questionnaire-2", true, true, Seq(qId1, qId2, qId3)))
+    //  registrationService.registerUser(UserRegisterRequest("1", "1", Gender.MALE, 26, None))
+    triggerService.addTrigger(CreateTriggerRequest("Trigger Name", id1, None, Some(SocialNetworkTrigger(true)), None))
+    questionsService.addQuestionnaireTo(userService.getAllUser()(0).userId, id1)
+    questionsService.addQuestionnaireTo(userService.getAllUser()(0).userId, id2)
+  } match {
+    case Failure(e) =>
+      println("*" * 50)
+      println(e)
+      println("*" * 50)
+  }
 
   @RequestMapping(method = Array(POST), value = Array("login"))
   @ResponseBody
